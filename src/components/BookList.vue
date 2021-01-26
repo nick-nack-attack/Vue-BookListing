d60ceaf032b2a2b3835ce7c74d0cb9192d940bc0
Nick Nack Attack <nickfarni001@gmail.com>
2021-01-25 17:56:54 -0700

    Module 4

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -4,14 +4,17 @@
       {{title}}
     </h1>
     <ul>
-      <li v-for="book in books">{{book.title}}: {{book.author}}</li>
+      <book-item v-for="book in books" :book=book></book-item>
     </ul>
   </div>
 </template>
 
 <script>
+  import BookItem from "./BookItem";
+
   export default {
     name: 'BookList',
+    components: {BookItem},
     data() {
       return {
         title: 'All Books',
@@ -19,10 +22,14 @@
           {title: 'Self-Reliance', author: 'Ralph Waldo Emerson'},
           {title: 'American Gods', author: 'Neil Gaiman'},
           {title: 'Amusing Ourselves to Death', author: 'Neil Postman'},
-        ]
+        ],
+        components: {
+          BookItem
+        },
       };
     },
   };
+
 </script>
 
 <style>
